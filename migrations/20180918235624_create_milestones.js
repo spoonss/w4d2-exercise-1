56e7bec1a9f1854e2e1d00648d0bfbe639006f07
Charles Wong <nok.wong@gmail.com>
2018-09-19 00:32:33 +0000

    knex migration, added primary key for milestones table

---
+++ migrations/20180918235624_create_milestones.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('milestones', function(table){
+          table.increments('id').primary();
+          table.string('description');
+          table.date('date_achieved');
+        })
+      ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('milestones')
+      ])
+};
+
